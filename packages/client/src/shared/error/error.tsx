0b265b59d885086da7d4090525c803e25fea3ed5
andreeva_en <EN.Andreeva@abr.ru>
2025-01-30 17:48:48 +0300

    add server error page

---
+++ packages/client/src/shared/error/error.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+import { Button } from '../button'
+
+import styles from './styles.module.css'
+
+type TErrorProps = {
+  description: string
+  text: string
+  children: React.ReactNode
+}
+
+export const Error: React.FC<TErrorProps> = ({
+  description,
+  text,
+  children,
+}) => {
+  return (
+    <div className={styles.pageContentContainer}>
+      <div className={styles.textContentContainer}>
+        <div className={styles.titleContainer}>{children}</div>
+
+        <div className={styles.descriptionContainer}>
+          <h1 className={styles.description}>{description}</h1>
+          <p className={styles.text}>{text}</p>
+        </div>
+      </div>
+
+      <Button size="l" color="contrast" label="Играть дальше" />
+    </div>
+  )
+}
