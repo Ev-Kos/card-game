52f5076055c9768351495470cc70e8f533d7b374
Evgeniia Kostyria <zhenya241191@gmail.com>
2025-02-08 13:45:55 +0300

    убрала ошибку в слове opacity

--- packages/client/src/shared/card/Card.ts
+++ packages/client/src/shared/card/Card.ts
@@ -6,19 +6,19 @@ export const Card = (
   image: HTMLImageElement,
   x: number,
   y: number,
-  isOpasity: boolean,
+  isOpacity: boolean,
 ) => {
   if (!isTrumpCard) {
     ctx.imageSmoothingEnabled = true
     ctx.drawImage(image, x, y, CARD_WIDTH, CARD_HEIGHT)
   }
-  if (isTrumpCard && !isOpasity) {
-    if (!isOpasity) {
+  if (isTrumpCard && !isOpacity) {
+    if (!isOpacity) {
       ctx.imageSmoothingEnabled = true
       ctx.drawImage(image, x - CARD_WIDTH - 10, y + 3, CARD_WIDTH, CARD_HEIGHT)
     }
   }
-  if (isTrumpCard && isOpasity) {
+  if (isTrumpCard && isOpacity) {
     ctx.imageSmoothingEnabled = true
     ctx.drawImage(image, x + 3, y + 3, CARD_WIDTH, CARD_HEIGHT)
     ctx.fillStyle = 'rgba(211, 210, 210, 0.5)'
