e97ff26386d88281badc6408bb5cd321040b3a07
Artem Bakhmatov <ip.bakhmatov@mail.ru>
2025-02-08 13:41:56 +0300

    Правки по роутеру

--- packages/client/src/router/index.tsx
+++ packages/client/src/router/index.tsx
@@ -3,14 +3,14 @@ import { type RouteObject } from 'react-router-dom'
 import { NotFoundErrorPage, ServerErrorPage } from '../pages'
 
 const arrayRoutes: RouteObject[] = [
-  { path: '/', element: <>any text</> },
-  { path: '/home', element: <>any text</> },
-  { path: '/registration', element: <>any text</> },
-  { path: '/forum', element: <>any text</> },
-  { path: '/forum/:id', element: <>any text</> },
-  { path: '/game', element: <>any text</> },
-  { path: '/leaderboard', element: <>any text</> },
-  { path: '/profile', element: <>any text</> },
+  { path: '/', element: <>LoginPage</> },
+  { path: '/home', element: <>HomePage</> },
+  { path: '/registration', element: <>RegistrationPage</> },
+  { path: '/forum', element: <>ForumPage</> },
+  { path: '/forum/:id', element: <>ForumTopicPage</> },
+  { path: '/game', element: <>PageGame</> },
+  { path: '/leaderboard', element: <>LeaderboardPage</> },
+  { path: '/profile', element: <>ProfilePage</> },
   { path: '/*', element: <NotFoundErrorPage /> },
   { path: '/error', element: <ServerErrorPage /> },
 ]
