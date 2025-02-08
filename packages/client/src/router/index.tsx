24033ea06205422f61f69d95c3d7edfd9ef16e2f
Artem Bakhmatov <ip.bakhmatov@mail.ru>
2025-02-08 13:17:24 +0300

    Правки по роутеру

--- packages/client/src/router/index.tsx
+++ packages/client/src/router/index.tsx
@@ -1,28 +1,17 @@
 import { type RouteObject } from 'react-router-dom'
 
-import {
-  LoginPage,
-  RegistrationPage,
-  ForumPage,
-  ForumTopicPage,
-  GamePage,
-  HomePage,
-  LeaderboardPage,
-  NotFoundPage,
-  ProfilePage,
-  ServerErrorPage,
-} from '../pages'
+import { NotFoundErrorPage, ServerErrorPage } from '../pages'
 
 const arrayRoutes: RouteObject[] = [
-  { path: '/', element: <LoginPage /> },
-  { path: '/home', element: <HomePage /> },
-  { path: '/registration', element: <RegistrationPage /> },
-  { path: '/forum', element: <ForumPage /> },
-  { path: '/forum/:id', element: <ForumTopicPage /> },
-  { path: '/game', element: <GamePage /> },
-  { path: '/leaderboard', element: <LeaderboardPage /> },
-  { path: '/profile', element: <ProfilePage /> },
-  { path: '/*', element: <NotFoundPage /> },
+  // { path: '/', element: {/* <LoginPage /> */} },
+  // { path: '/home', element: {/* <HomePage /> */} },
+  // { path: '/registration', element: {/* <RegistrationPage /> */} },
+  // { path: '/forum', element: {/* <ForumPage /> */} },
+  // { path: '/forum/:id', element: {/* <ForumTopicPage /> */} },
+  // { path: '/game', element: {/* <GamePage /> */} },
+  // { path: '/leaderboard', element: {/* <LeaderboardPage /> */} },
+  // { path: '/profile', element: {/* <ProfilePage /> */} },
+  { path: '/*', element: <NotFoundErrorPage /> },
   { path: '/error', element: <ServerErrorPage /> },
 ]
 
