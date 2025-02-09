696cf3dc09839383501a27a1af70ef52f779944e
Evgeniia Kostyria <zhenya241191@gmail.com>
2025-02-09 20:26:55 +0300

    input range

--- packages/client/src/entities/tooles-game/tooles-game.tsx
+++ packages/client/src/entities/tooles-game/tooles-game.tsx
@@ -1,7 +1,14 @@
+import { ChangeEvent, useState } from 'react'
 import { InputRange } from '../../shared/input-range/input-range'
 import styles from './styles.module.css'
 
 export const ToolesGame = () => {
+  const [valueSound, setValueSound] = useState(100)
+
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setValueSound(Number(e.target.value))
+  }
+
   return (
     <div className={styles.container}>
       <p className={styles.name}>Настройки</p>
@@ -9,7 +16,7 @@ export const ToolesGame = () => {
         <p className={styles.toolsName}>Звук</p>
         <div className={styles.tool}>
           <p className={styles.toolName}>Музыка</p>
-          <InputRange />
+          <InputRange onChange={onChange} valueSound={valueSound} />
         </div>
         <div className={styles.tool}>
           <p>Эффекты</p>
--- packages/client/src/shared/input-range/input-range.tsx
+++ packages/client/src/shared/input-range/input-range.tsx
@@ -1,13 +1,33 @@
+import { useEffect, useState } from 'react'
+import { SoundIcon } from './soundIcon'
 import styles from './styles.module.css'
 
 type TInputRangeProps = {
   valueSound?: number
 } & React.InputHTMLAttributes<HTMLInputElement>
 
-export const InputRange = ({ ...props }: TInputRangeProps) => {
+export const InputRange = ({ valueSound, ...props }: TInputRangeProps) => {
+  const [background, setBackground] = useState('')
+
+  useEffect(() => {
+    setBackground(
+      `linear-gradient(to right, #350f68 ${valueSound}%,#b38ff1 ${valueSound}% `,
+    )
+  }, [valueSound])
+
   return (
     <div className={styles.container}>
-      <input type="range" className={styles.input} {...props} />
+      <SoundIcon />
+      <input
+        type="range"
+        className={styles.input}
+        {...props}
+        min={0}
+        max={100}
+        value={valueSound}
+        style={{ background: background }}
+      />
+      <p className={styles.valueSound}>{`${valueSound}%`}</p>
     </div>
   )
 }
