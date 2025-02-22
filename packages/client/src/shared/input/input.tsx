7d9f4f743ce1e102bcae33ac8415d48a4b229663
Evgeniia Kostyria <zhenya241191@gmail.com>
2025-02-22 18:59:47 +0300

    сохранение данных формы

--- packages/client/src/shared/input/input.tsx
+++
@@ -1,9 +0,0 @@
-import { FC, InputHTMLAttributes } from 'react'
-
-type TInputProps = InputHTMLAttributes<HTMLInputElement> & {
-  className: string
-}
-
-export const Input: FC<TInputProps> = ({ className, ...props }) => {
-  return <input className={className} {...props} />
-}
