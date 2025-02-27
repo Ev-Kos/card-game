05c77c49fe9564cd917b05fc6a1231377f929ff1
Evgeniia Kostyria <zhenya241191@gmail.com>
2025-02-27 16:29:42 +0300

    fix input

---
+++ packages/client/src/shared/input/input.tsx
@@ -0,0 +1,48 @@
+import classNames from 'classnames'
+
+import type React from 'react'
+
+import styles from './styles.module.css'
+
+export type TInputProps = {
+  isError?: boolean
+  isProfile?: boolean
+  isComment?: boolean
+} & React.InputHTMLAttributes<HTMLInputElement>
+
+export const Input = ({
+  title,
+  isError,
+  isProfile,
+  isComment,
+  ...props
+}: TInputProps) => {
+  const className = classNames(styles.input, {
+    [styles.inputError]: isError,
+  })
+
+  const classNameProfile = classNames(styles.inputProfile, {
+    [styles.inputError]: isError,
+  })
+
+  const classNameComment = classNames(styles.inputComment, {
+    [styles.inputError]: isError,
+  })
+
+  return (
+    <div className={styles.inputContainer}>
+      {title && <p className={styles.inputLabel}>{title}</p>}
+      <input
+        type="text"
+        className={
+          isProfile
+            ? classNameProfile
+            : isComment
+              ? classNameComment
+              : className
+        }
+        {...props}
+      />
+    </div>
+  )
+}
