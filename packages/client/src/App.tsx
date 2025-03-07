6fe9651b46a228429404fd71fda731b3c1ba2528
Evgeniia Kostyria <zhenya241191@gmail.com>
2025-03-07 13:23:13 +0300

    fix model

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,22 +1,23 @@
 import { RouterProvider } from 'react-router-dom'
 import { router } from './shared/routes/routes'
+import { useEffect } from 'react'
 
 function App() {
-  // useEffect(() => {
-  //   const fetchServerData = async () => {
-  //     try {
-  //       const serverUrl = __SERVER_URL__ || 'http://localhost:'
-  //       const url = `${serverUrl}${__SERVER_PORT__}`
-  //       const response = await fetch(url)
-  //       const data = await response.json()
-  //       console.info(data)
-  //     } catch (error) {
-  //       console.error(error)
-  //     }
-  //   }
+  useEffect(() => {
+    const fetchServerData = async () => {
+      try {
+        const serverUrl = __SERVER_URL__ || 'http://localhost:'
+        const url = `${serverUrl}${__SERVER_PORT__}`
+        const response = await fetch(url)
+        const data = await response.json()
+        console.info(data)
+      } catch (error) {
+        console.error(error)
+      }
+    }
 
-  //   fetchServerData()
-  // }, [])
+    fetchServerData()
+  }, [])
 
   return <RouterProvider router={router} />
 }
