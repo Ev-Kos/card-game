803ff41c390a09230e246fe232004cfd80f6ce30
Evgeniia Kostyria <zhenya241191@gmail.com>
2025-03-21 15:14:09 +0300

    fix rollup/parseAst

---
+++ packages/client/server/index.mjs
@@ -0,0 +1,55 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import express from 'express';
+import path from 'path';
+import fs from 'fs/promises';
+import { createServer as createViteServer } from 'vite';
+const port = process.env.CLIENT_PORT || 3000;
+const __dirname = path.resolve();
+const clientPath = __dirname;
+const isDev = process.env.NODE_ENV === 'development';
+async function createServer() {
+    const app = express();
+    let vite;
+    if (isDev) {
+        vite = await createViteServer({
+            server: { middlewareMode: true },
+            root: clientPath,
+            appType: 'custom',
+        });
+        app.use(vite.middlewares);
+    }
+    else {
+        app.use(express.static(path.join(clientPath, 'dist/client'), { index: false }));
+    }
+    app.get('*', async (req, res, next) => {
+        const url = req.originalUrl;
+        try {
+            let render;
+            let template;
+            if (vite) {
+                template = await fs.readFile(path.resolve(clientPath, 'index.html'), 'utf-8');
+                template = await vite.transformIndexHtml(url, template);
+                render = (await vite.ssrLoadModule(path.join(clientPath, 'src/entry-server.tsx'))).render;
+            }
+            else {
+                template = await fs.readFile(path.join(clientPath, 'dist/client/index.html'), 'utf-8');
+                const pathToServer = path.join(clientPath, 'dist/server/entry-server.js');
+                render = (await import(pathToServer)).render;
+            }
+            const { html: appHtml } = await render(req);
+            const html = template.replace(`<!--ssr-outlet-->`, appHtml);
+            res.status(200).set({ 'Content-Type': 'text/html' }).end(html);
+        }
+        catch (e) {
+            if (isDev && vite) {
+                vite.ssrFixStacktrace(e);
+            }
+            next(e);
+        }
+    });
+    app.listen(port, () => {
+        console.log(`Client is listening on port: ${port}`);
+    });
+}
+createServer();
--- packages/client/server/index.mts
+++ packages/client/server/index.mts
@@ -8,7 +8,8 @@ import fs from 'fs/promises'
 import { createServer as createViteServer, ViteDevServer } from 'vite'
 
 const port = process.env.CLIENT_PORT || 3000
-const clientPath = path.join(__dirname, '..')
+const __dirname = path.resolve()
+const clientPath =  __dirname
 const isDev = process.env.NODE_ENV === 'development'
 
 async function createServer() {
