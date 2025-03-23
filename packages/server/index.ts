10daa6493d6682c2f2a4ae2f254d0ff624d682c8
Evgeniia Kostyria <zhenya241191@gmail.com>
2025-03-23 15:37:31 +0300

    установка orm, настройка запуска сервера

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,20 +1,26 @@
 import dotenv from 'dotenv'
 import cors from 'cors'
-dotenv.config()
+
+dotenv.config({ path: '../../.env' })
 
 import express from 'express'
-import { createClientAndConnect } from './db'
+import { sequelize } from './db'
 
-const app = express()
-app.use(cors())
 const port = Number(process.env.SERVER_PORT) || 3001
 
-createClientAndConnect()
+sequelize
+  .sync({ force: true })
+  .then(() => {
+    const app = express()
+    app.use(cors())
+    app.get('/', (_, res) => {
+      res.json('👋 Howdy from the server :)')
+    })
 
-app.get('/', (_, res) => {
-  res.json('👋 Howdy from the server :)')
-})
-
-app.listen(port, () => {
-  console.log(`  ➜ 🎸 Server is listening on port: ${port}`)
-})
+    app.listen(port, () => {
+      console.log(`  ➜ 🎸 Server is listening on port: ${port}`)
+    })
+  })
+  .catch(err => {
+    console.log(err)
+  })
